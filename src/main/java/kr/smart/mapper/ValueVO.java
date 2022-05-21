16f77554511265b432bc000f2c9d31ec10dd4fd1
smhrd <smhrd@220.80.165.73>
2022-05-21 17:47:22 +0900

    220521 DBtest Value inesert test

---
+++ src/main/java/kr/smart/mapper/ValueVO.java
@@ -0,0 +1,17 @@
+package kr.smart.mapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ValueVO {
+	private String v_seq ;
+	private String d_seq ;
+	private String v_weight ;
+	private String v_signdate ;
+	
+	
+}
