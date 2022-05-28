1bdf29e3ab0a45081520930894fa95308e7c0384
smhrd <smhrd@220.80.165.73>
2022-05-28 17:51:05 +0900

    value change

--- src/main/java/kr/smart/keepers/ValueController.java
+++ src/main/java/kr/smart/keepers/ValueController.java
@@ -3,7 +3,6 @@ package kr.smart.keepers;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,10 +20,11 @@ public class ValueController {
 		
 		String d_seq = request.getParameter("d_seq");
 		String v_weight = request.getParameter("v_weight");
+		String v_bat = request.getParameter("v_bat");
 		
-		ValueVO vo = new ValueVO("default", d_seq, v_weight, "default");
-		
-		mapper.insertValue(vo);
+		ValueVO vo = new ValueVO("default", d_seq, v_weight, "default", v_bat);
+		System.out.println(vo.toString());
+		//mapper.insertValue(vo);
 		
 		System.out.println("==[센서값 입력 종료]==");
 		
