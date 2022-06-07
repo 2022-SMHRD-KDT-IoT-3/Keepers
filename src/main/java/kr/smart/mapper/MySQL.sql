6d7d34c0bad186642f65d26982b8941112b29aaa
dohyeongHeo <dohyeong.heo@gmail.com>
2022-06-07 11:52:58 +0900

    테스트페이지 추가

--- src/main/java/kr/smart/mapper/MySQL.sql
+++ src/main/java/kr/smart/mapper/MySQL.sql
@@ -10,3 +10,17 @@ select * from k_value;
 select * from k_member where m_id = "checker"
 
 insert into k_value(d_seq, v_weight, v_bat) values () 
+
+
+
+insert into k_value values(default, 1, 50 ,default, 70)
+
+insert into k_value values(default, 1, 55 ,default, 70)
+
+insert into k_value values(default, 1, 60 ,default, 70)
+
+insert into k_value values(default, 1, 65 ,default, 70)
+
+insert into k_value values(default, 1, 70 ,default, 70)
+
+
